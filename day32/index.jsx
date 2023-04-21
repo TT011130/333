b2df4672f0d587067539ef14269d8c63134589e2
TT011130 <65943266+TT011130@users.noreply.github.com>
2023-04-21 17:25:16 +0700

    day32

--- day32/index.jsx
+++ day32/index.jsx
@@ -1,41 +1,45 @@
-const app = document.querySelector("#root");
-
-const h1 = document.createElement("h1");
-
-h1.textContent = "Hello";
-
-app.append(h1);
+//const app = document.querySelector("#root");
+//
+//const h1 = document.createElement("h1");
+//
+//h1.textContent = "Hello";
+//
+//app.append(h1);
 //tạo root container để chứa ứng dụng react
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 //tạo phần tử
 //react.createElement(tagName, props, children)
-const h1Element = React.createElement("h1", {
+//const h1Element = React.createElement("h1", {
     //một số thuộc tính đặc biệt thì đổi tên
-    id: "heading",
-    className: "heading",
-    children: "Hello React Element",
+    //id: "heading",
+    //className: "heading",
+    //children: "Hello React Element",
     //các thuộc tính thì chuyển đổi thành dạng camelCase
-    onClick() {
-        console.log("hahahahaha");
-    },
-    onMouseOver() {
-        console.log("mouse over");
-    }
-});
+    //onClick() {
+    //    console.log("hahahahaha");
+    //},
+    //onMouseOver() {
+    //    console.log("mouse over");
+    //}
+//});
 
-const imgElement = React.createElement("img", {
-    src: "https://taimienphi.vn/tmp/cf/aut/Uhun-IbFB-MnGE-DiPL-OCTB-hinh-dep-1.jpg",
-    alt: "ảnh đẹp",
-});
+//const imgElement = React.createElement("img", {
+    //src: "https://taimienphi.vn/tmp/cf/aut/Uhun-IbFB-MnGE-DiPL-OCTB-hinh-dep-1.jpg",
+    //alt: "ảnh đẹp",
+//});
+
+//<div>
+   // <h1>Hello React</h1>
+//</div>
 
 //thẻ cha chứa tất cả các thẻ con
-const divElement = React.createElement("div", {
-    children: [h1Element, imgElement]
-});
+//const divElement = React.createElement("div", {
+    //children: [h1Element, imgElement]
+//});
 
 //hiển thị element trên trình duyệt
-root.render(divElement);
+//root.render(divElement);
 
 //jsx -javascriptxml
 //mở rộng cú pháp javascript và cho phép viết trực tiếp mã html vào trong js
@@ -46,13 +50,23 @@ const div = (
         </h1>
         <img
         src = "https://taimienphi.vn/tmp/cf/aut/Uhun-IbFB-MnGE-DiPL-OCTB-hinh-dep-1.jpg"
-        alt = "ảnh đẹp"></img>
+        alt = "ảnh đẹp">
+        </img>
     </div>
 );
 
+//hiển thị element trên trình duyệt
 root.render(div);
 
 
-//hiệu ứng
-//gợi ý
-//tự tìm kiếm
+//Hạn chế: -hiệu ứng
+//         -gợi ý
+//         -tự tìm kiếm, cài đặt, cấu hình các công cụ
+
+// bộ công cụ - Build tool
+//Bao gồm sẵn các thư viện và cấu hình cần thiết
+// Tối ưu hóa các thao tác quản lý, cài đặt thư viện
+//Tối ưu hóa mã cho production
+
+// Create React App (chậm, nặng)
+//Vite (nhanh hơn, gọn nhẹ hơn, hiệu năng tốt hơn)
